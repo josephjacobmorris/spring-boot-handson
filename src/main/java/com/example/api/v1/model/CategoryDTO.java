cbf467a0ba29b5011a326249eb314dff559ef2af
Joseph Jacob <josephjacob1998@gmail.com>
2023-02-21 10:27:22 +0530

    add Category domain , Data Transfer Object and Repository

---
+++ src/main/java/com/example/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.example.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/example/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
